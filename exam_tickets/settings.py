b57fb69a0c2ed6695890eccaddbb1f15ba13144f
Rinat Aghayev <rinatagayev007@mail.ru>
2026-02-02 17:02:33 +0400

    Add custom domain to ALLOWED_HOSTS

--- exam_tickets/settings.py
+++ exam_tickets/settings.py
@@ -14,7 +14,10 @@ SECRET_KEY = 'django-insecure-dev-key-change-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'www.ТВОЙДОМЕН.ru',
+    '.up.railway.app',
+]
 
 # Application definition
 INSTALLED_APPS = [
